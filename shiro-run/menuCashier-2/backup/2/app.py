2d9a8c7190803c99ac755fac044b3ff4a91fe965
Shiro <540656@student.fontys.nl>
2023-11-06 12:01:22 +0100

    Shro see comments
    pizzeria-project\shiro-run\menuCashier-2

--- shiro-run/menuCashier-2/backup/2/app.py
+++ shiro-run/menuCashier-2/backup/2/app.py
@@ -12,16 +12,30 @@ from flask import Flask, render_template, request
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 
+
 app = Flask(__name__,)
 app.static_folder = 'static'
 
+
+
+
+
+
 @app.route('/')
 def homepage():
+    #link=url_for('home')
     return redirect('/casier')
+
 
 @app.route('/casier')
 def hello_Temperature():
+    #link = url_for('casier') 
     return render_template('menuCashier.html')
+
+
+#
+
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -31,6 +45,7 @@ def submit():
         pizza3 = request.form['quantity3']
         pizza4 = request.form['quantity4']
         pizza5 = request.form['quantity5']
+        # drinks
         drinks1 = request.form['quantity6']
         drinks2 = request.form['quantity7']
         drinks3 = request.form['quantity8']
@@ -40,6 +55,7 @@ def submit():
         with open('data.csv', 'a', newline='') as file:
             csv_writer = csv.writer(file)
             csv_writer.writerow([pizza1, pizza2, pizza3, pizza4, pizza5, drinks1, drinks2, drinks3, drinks4, drinks5])
+            
 
         return '''
         <h1>Data stored successfully!</h1>
@@ -48,11 +64,33 @@ def submit():
         </form>
         '''
 
+ 
+
+#@app.route('/orders')
+#def display_orders():
+#    with open('data.csv', 'r') as file:
+#        csv_reader = csv.reader(file)
+#        data_list = list(csv_reader)
+#
+#    # Calculate the average for each column
+#    averages = []
+#    for i in range(len(data_list[0])):
+#        column_values = [float(row[i]) for row in data_list[1:] if row[i] != '']  # Exclude empty values
+#        column_average = sum(column_values) / len(column_values)
+#        averages.append(column_average)
+#
+#    num_columns = len(data_list[0])  # Calculate the number of columns
+#
+#    return render_template('data.html', data=data_list, averages=averages, num_columns=num_columns)
+#
+
+
 @app.route('/empty')
 def empty_csv():
     with open('data.csv', 'w') as file:
         file.truncate(0)
 
+    #return 'CSV file emptied successfully!'
     return '''
     <h1>CSV file emptied successfully!</h1>
     <h2>Wait 10 seconds!</h2>
@@ -62,11 +100,17 @@ def empty_csv():
     <form action="/data" method="get">
         <button type="submit">Go to data</button>
     </form>
+
+
     '''
 
+
+#
+# write to csv
+ 
 def write_data_to_csv():
     global pizza1, pizza2, pizza3, pizza4, pizza5, drinks1, drinks2, drinks3, drinks4, drinks5
-    data_file_path = 'data.csv'
+    data_file_path = 'data.csv'  # Relative path to the file
 
     while True:
         if not os.path.exists(data_file_path):
@@ -75,6 +119,11 @@ def write_data_to_csv():
         with open(data_file_path, 'a', newline='') as file:
             file.write(f"{pizza1}, {pizza2}, {pizza3}, {pizza4}, {pizza5}, {drinks1}, {drinks2}, {drinks3}, {drinks4}, {drinks5}\n")
         time.sleep(10)
+
+
+# --------------
+# main program
+# --------------
 
 if __name__ == "__main__":
     app.run(debug=False)
